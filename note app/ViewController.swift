9a627bd9b7db49efddd3a621ba7b7c70d1ea6e00
amjadkheder <amjad.kheder@gmail.com>
2022-12-17 08:14:34 +0300

    new project

---
+++ note app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  note app
+//
+//  Created by Amjad on 23/05/1444 AH.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
